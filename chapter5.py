198a37c37741fa7206a312c3509f57df7d7df1b0
Max <cailige2012@163.com>
2016-02-01 20:52:01 +0800

    20160201

--- chapter5.py
+++ chapter5.py
@@ -1,45 +1,24 @@
 __author__ = 'Administrator'
-import pickle
-import os
+import newinter34.temp.chapter5sanitize
 # os.chdir('D:/Code/resources/chapter5')
-James = []
-Sarah = []
-Julie = []
-Mikey = []
-try:
-    # with open('D:/Code/resources/chapter5/james.txt','rb')as james_file,open('D:/Code/resources/chapter5/julie.txt','rb')as julie_file,open('D:/Code/resources/chapter5/mikey.txt','rb')as mikey_file,open('D:/Code/resources/chapter5/sarah.txt','rb')as sarah_file:
-    #     James = pickle.load(james_file)
-    #     Sarah = pickle.load(sarah_file)
-    #     Julie = pickle.load(julie_file)
-    #     Mikey = pickle.load(mikey_file)
-    Jamesdata = open('D:/Code/resources/chapter5/james.txt','r')
-    Sarahdata = open('D:/Code/resources/chapter5/sarah.txt','r')
-    Juliedata = open('D:/Code/resources/chapter5/julie.txt','r')
-    Mikeydata = open('D:/Code/resources/chapter5/mikey.txt','r')
-    James = Jamesdata.read().strip().split(',')
-    Sarah = Sarahdata.read().strip().split(',')
-    Julie = Juliedata.read().strip().split(',')
-    Mikey = Mikeydata.read().strip().split(',')
-    print(James)
-    print(sorted(James))
-    print(Sarah)
-    print(sorted(Sarah))
-    print(Julie)
-    print(sorted(Julie))
-    print(Mikey)
-    print(sorted(Mikey))
-    # James.append()
-except IOError as err:
-    print('File error:'+str(err))
-except pickle.PickleError as perr:
-    print('Pickle error:'+str(perr))
-finally:
-    Jamesdata.close()
-    Juliedata.close()
-    Mikeydata.close()
-    Sarahdata.close()
-    pass
-    #
-    # print(Sarah)
-    # print(Julie)
-    # print(Mikey)
+def dataopen(dataurl):
+    try:
+        with open(dataurl) as daf:
+            data = daf.readline()
+        return(data.strip().split(','))
+    except IOError as ioerr:
+        print('File error:'+str(ioerr))
+        return (None)
+James = dataopen('D:/Code/resources/chapter5/james.txt')
+Sarah = dataopen('D:/Code/resources/chapter5/sarah.txt')
+Julie = dataopen('D:/Code/resources/chapter5/julie.txt')
+Mikey = dataopen('D:/Code/resources/chapter5/mikey.txt')
+newJames = sorted(set([newinter34.temp.chapter5sanitize.chapter5sanitize(j) for j in James]))
+newSarah = sorted(set([newinter34.temp.chapter5sanitize.chapter5sanitize(j) for j in Sarah]))
+newJulie = sorted(set([newinter34.temp.chapter5sanitize.chapter5sanitize(j) for j in Julie]))
+newMikey = sorted(set([newinter34.temp.chapter5sanitize.chapter5sanitize(j) for j in Mikey]))
+print(newJames[0:3])
+print(newSarah[0:3])
+print(newJulie[0:3])
+print(newMikey[0:3])
+
